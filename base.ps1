9e7127c67042aea541cc00f158de2e42a63f2bee
Jack Muriwai <63270842+MuriwaiJ@users.noreply.github.com>
2023-07-30 20:09:39 +1200

    Add files via upload

---
+++ base.ps1
@@ -0,0 +1,19 @@
+﻿Set-Location "C:\users\JackM\Braemar Hospital\IT - Documents\General\Scripts\NewUserAccounts"
+
+Function Get-ScriptDirectory {
+if($psise){
+Split-Path $psise.CurrentFile.FullPath
+}
+else {
+$Global:PSScriptRoot
+}
+}
+
+# Uses Get-ScriptDirectory function to get all the folders and adds a dot 
+$(Get-ChildItem $(Get-ScriptDirectory) -Recurse -Filter "*BH*").FullName | ForEach-Object { . $_}  #% { . $_}
+
+
+
+
+
+
